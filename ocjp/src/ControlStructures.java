3a1dc8bcc1dfab200b46134290188fa56c645cac
tom-bewise-jp <tom@bewise.jp>
2023-02-23 15:28:03 +0900

    ocjp第3章まで

--- ocjp/src/ControlStructures.java
+++ ocjp/src/ControlStructures.java
@@ -1,4 +1,27 @@
 
 public class ControlStructures {
-
+	public class Signal {
+		public static final String RED = "Red";
+		public static final String YELLOW = "Yellow";
+		public static final String GREEN = "Green";
+	}
+	public static void main(String[] args) {
+		checkSignal("Blue");
+	}
+	
+	public static void checkSignal(String signal) {
+		switch (signal) {
+		case Signal.RED:
+			// 停止する
+			break;
+		case Signal.YELLOW:
+			// 安全に停止できるか判断する
+			break;
+		case Signal.GREEN:
+			// 安全を確認の上進む
+			break;
+		default:
+			// 何らかの障害が発生しているので停止する
+		}
+	}
 }
