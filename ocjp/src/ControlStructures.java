88eb884d03d877e55d65db77a8fd2d66234d78d0
tom-bewise-jp <tom@bewise.jp>
2023-02-26 00:23:41 +0900

    ocjp 第4章 第1稿

--- ocjp/src/ControlStructures.java
+++ ocjp/src/ControlStructures.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 
 public class ControlStructures {
 	public class Signal {
@@ -7,6 +8,9 @@ public class ControlStructures {
 	}
 	public static void main(String[] args) {
 		checkSignal("Blue");
+		whileLoopExample();
+		forLoopExample();
+		getPrimes();
 	}
 	
 	public static void checkSignal(String signal) {
@@ -23,5 +27,64 @@ public class ControlStructures {
 		default:
 			// 何らかの障害が発生しているので停止する
 		}
+	}
+	
+	public static void whileLoopExample() {
+		int i = 1;
+		int sum = 0;
+		while (i <= 10) {
+			sum += i;
+			++i;
+		}
+		System.out.println("sum: " + sum);
+		
+		i = 1;
+		sum = 0;
+		while (i <= 10)
+			sum += i++;
+		System.out.println("sum: " + sum);
+	}
+	
+	public static void forLoopExample() {
+		int sum = 0;
+		for (int i = 1; i <= 10; ++i) {
+			sum += i;
+		}
+		System.out.println("sum: " + sum);
+
+		sum = 0;
+		for (int i = 1, j = 1; i <= 10; ++i, ++j) {
+			sum += i + j;
+		}
+		System.out.println("sum: " + sum);
+		
+		sum = 0;
+		for (int i = 1; i <= 10; sum += i, ++i) ;
+		System.out.println("sum: " + sum);
+		
+		sum = 0;
+		for (int i = 1; i <= 10 && (sum += i) > 0; ++i) ;
+		System.out.println("sum: " + sum);
+	}
+	
+	public static void getPrimes() {
+		boolean[] table = new boolean[101];
+		for (int i = 2; i < 100; ++i) {
+			table[i] = true;
+		}
+		for (int i = 2; i < 100; ++i) {
+			if (!table[i])
+				continue;
+			for (int j = i + i; j < 100; j += i) {
+				table[j] = false;
+			}
+		}
+		System.out.println("primes:");
+		System.out.print("2");
+		for (int i = 3; i < 100; ++i) {
+			if (table[i])
+				System.out.print(", " + i);
+		}
+		System.out.println();
 	}
 }
