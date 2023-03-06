ef968494e20f88dbaefe0a3e47de6eb4b40c8acd
tom-bewise-jp <tom@bewise.jp>
2023-03-06 22:18:34 +0900

    ocjp 第6章 インスタンスとクラス関連

--- ocjp/src/StaticAndInstance.java
+++ ocjp/src/StaticAndInstance.java
@@ -1,4 +1,19 @@
 
 public class StaticAndInstance {
+	static final int a = 1;
+	final int b;
+	
+	static void sayHello(String s) {
+		System.out.println("Hello, " + s);
+	};
+	
+	void sayHello(int n) {
+		for (int i = 0; i < n; ++i) {
+			System.out.println("Hello, World!");
+		}
+	}
 
+	StaticAndInstance(int i) {
+		b = i;
+	}
 }
